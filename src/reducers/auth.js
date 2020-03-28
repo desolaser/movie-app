f61baa0e9c68258baa623e7b141df67df26b0706
desolaser <178.felipe@gmail.com>
2020-03-28 17:49:02 -0300

    Add auth reducer

---
+++ src/reducers/auth.js
@@ -0,0 +1,9 @@
+const authReducer = (state = false, action) => {
+    switch(action.type) {
+        case 'LOGIN' : return true;
+        case 'LOGOUT' : return false;
+        default: return state;
+    }
+}
+
+export default authReducer;
