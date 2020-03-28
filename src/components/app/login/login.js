27964f16b18787c27c9bd0a092e0c8361906df06
desolaser <178.felipe@gmail.com>
2020-03-28 19:33:29 -0300

    Create login component

---
+++ src/components/app/login/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { login, logout } from '../../actions'
+
+function Login(props) {
+  return (
+    <div className="login">
+
+    </div>
+  );
+}
+
+export default Login;
