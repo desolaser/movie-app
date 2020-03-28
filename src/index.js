187b5d71e23eaf1be0fada581aed556f12043a1e
desolaser <178.felipe@gmail.com>
2020-03-28 17:38:26 -0300

    Change App from location

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './js/components/app';
 import * as serviceWorker from './serviceWorker';
+
+
+/* 
+This is to bind the store to redux chrome extension
+
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
+*/
 
 ReactDOM.render(
   <React.StrictMode>
