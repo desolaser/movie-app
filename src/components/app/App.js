eb1c4c5bb8568327b17da04d4ba157ae49fc588e
desolaser <178.felipe@gmail.com>
2020-03-28 19:02:40 -0300

    Add store hooks and login message and button in App

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { login, logout } from '../../actions'
 
 function App() {
+  const auth = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
+  
+
+  function handleChange() {
+    if (auth) {
+      dispatch(logout())
+    } else {
+      dispatch(login())
+    }
+  }
+
   return (
     <div className="App">
-      Hello
+      <h2>You are currently logged {auth ? "in" : "out"}</h2>
+      <button onClick={handleChange}>
+        {auth ? "Logout": "Login"}
+      </button>
     </div>
   );
 }
