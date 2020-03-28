49bdb89494fb432fa6d24d0b5afdc522da246f47
desolaser <178.felipe@gmail.com>
2020-03-28 20:02:03 -0300

    Import bootstrap from index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+// import 'font-awesome/css/font-awesome.min.css';
 import App from './components/app';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
