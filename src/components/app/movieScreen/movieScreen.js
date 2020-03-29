fecdae77105d6e3bff7203fba21c5876439d2f08
desolaser <178.felipe@gmail.com>
2020-03-29 12:41:50 -0300

    Delete movieScreen.css and add Movie component to MovieScreen

--- src/components/app/movieScreen/movieScreen.js
+++ src/components/app/movieScreen/movieScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import './movieScreen.css'
 import SearchBar from './searchBar'
+import Movie from './movie'
 
 const MovieScreen = (props) => {
     const [input, setInput] = useState("")
@@ -10,11 +10,11 @@ const MovieScreen = (props) => {
     return(
         <div className="min-vh-100">
             <div className="justify-contents-center align-items-center">
-                <h1 className="text-white" align="center">Movies</h1>
+                <h1 className="text-white p-4" align="center">Movies</h1>
             </div>
             <SearchBar input={input} handleChange={handleChange}/>
             <div className="movies">
-
+                <Movie />
             </div>
         </div>
     )
