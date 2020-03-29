3c34e1b4c30f8f17a49ce0be7a06a5f42a612fa2
desolaser <178.felipe@gmail.com>
2020-03-29 12:14:29 -0300

    Change movie folder to movieScreen

---
+++ src/components/app/movieScreen/movieScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const movieScreen = (props) => {
+    return(
+        <div className="container min-vh-100">
+            <div className="col-sm-12 text-center">
+                <div className="row ">
+                    <h1 className="text-white">Movies</h1>
+                </div>
+                <div className="row">
+                    <input type="text" placeholder="Search" />
+                </div>
+                <div className="row">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default movieScreen
