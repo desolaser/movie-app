3eab87134517adabbb0d189e8b35ebce6487923d
desolaser <178.felipe@gmail.com>
2020-03-29 12:42:46 -0300

    Capitalize Movie component name

--- src/components/app/movieScreen/movie/movie.js
+++ src/components/app/movieScreen/movie/movie.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const movie = (props) => {
+const Movie = (props) => {
 	return(
 		<div >
 
@@ -8,4 +8,4 @@ const movie = (props) => {
 	);
 }
 
-export default movie;
+export default Movie;
