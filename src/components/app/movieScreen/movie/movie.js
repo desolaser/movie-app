528e7590290faad10385bee5b639e6170dd9453d
desolaser <178.felipe@gmail.com>
2020-03-30 22:29:54 -0300

    Add link to movieDetails in every Movie component

--- src/components/app/movieScreen/movie/movie.js
+++ src/components/app/movieScreen/movie/movie.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Movie = (props) => {
 	return(
 		<div className="col-sm-3">
 			<div className="card m-4 bg-dark">
 				<img className="card-image-top" src={props.image} alt={props.title}/>
-				<div className="card-body text-white">
-					<p className="card-text">
-						{props.title}
-					</p>
+				<div className="card-body text-white">						
+					<Link to={"/movies/" + props.id}>
+						<p className="card-text">
+							{props.title}
+						</p>
+					</Link>
 				</div>
 			</div>
 		</div>
