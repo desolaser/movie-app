9aa77fa745006d313d12e09a60e3192a87c0299b
desolaser <178.felipe@gmail.com>
2020-04-12 15:34:19 -0400

    Updates

---
+++ precache-manifest.25f737c9c7022bd7154decfca74d9037.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e19a351ddca14b42354f91392c5bbea9",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "9a553e454fd9ed0bbdcd",
+    "url": "/movie-app/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "caf7dda556ed072a1626",
+    "url": "/movie-app/static/css/3.e6f94020.chunk.css"
+  },
+  {
+    "revision": "e37696940ce57ca9d836",
+    "url": "/movie-app/static/css/4.5231ca64.chunk.css"
+  },
+  {
+    "revision": "616493a69e7a2743df17",
+    "url": "/movie-app/static/css/main.826849d7.chunk.css"
+  },
+  {
+    "revision": "9a553e454fd9ed0bbdcd",
+    "url": "/movie-app/static/js/2.e6e5c53f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-app/static/js/2.e6e5c53f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caf7dda556ed072a1626",
+    "url": "/movie-app/static/js/3.643a5a33.chunk.js"
+  },
+  {
+    "revision": "e37696940ce57ca9d836",
+    "url": "/movie-app/static/js/4.df606948.chunk.js"
+  },
+  {
+    "revision": "40bdaad6230925fd6f8b",
+    "url": "/movie-app/static/js/5.53bc1ec5.chunk.js"
+  },
+  {
+    "revision": "616493a69e7a2743df17",
+    "url": "/movie-app/static/js/main.50d9179d.chunk.js"
+  },
+  {
+    "revision": "dc319901a32fd939c737",
+    "url": "/movie-app/static/js/runtime-main.d41f86be.js"
+  }
+]);
