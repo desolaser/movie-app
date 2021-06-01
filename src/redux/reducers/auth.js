f5bb8573c9ee2b6e0c5e4484c2f832a886682528
Felipe Olavarría <178.felipe@gmail.com>
2021-06-01 10:01:15 -0400

    Delete redux actions and reducers

--- src/redux/reducers/auth.js
+++
@@ -1,9 +0,0 @@
-const authReducer = (state = false, action) => {
-    switch(action.type) {
-        case 'LOGIN' : return true;
-        case 'LOGOUT' : return false;
-        default: return state;
-    }
-}
-
-export default authReducer;
--- src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import authReducer from './auth'
-import { combineReducers } from 'redux'
-
-const rootReducer = combineReducers({
-    auth: authReducer
-})
-
-export default rootReducer
