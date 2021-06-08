0ea411c77c7fd6e5f9961f73488e6751b8e1ee70
Felipe Olavarría <178.felipe@gmail.com>
2021-06-08 13:13:16 -0400

    Add search button in topToolbar

--- src/components/topToolbar.js
+++ src/components/topToolbar.js
@@ -2,17 +2,25 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { logout } from '../redux/slice/authSlice'
 
-const TopToolbar = ({ search, handleChange }) => {
+const TopToolbar = ({ search, handleChange, handleSearch }) => {
 	const dispatch = useDispatch()
 	return (		
 		<div className="row justify-content-between bg-dark p-3">
-			<input
-				className="mb-2 col-xs-12 mb-md-0 col-md-4 p-2"
-				type="text" 
-				placeholder="Search"
-				value={search}
-				onChange={handleChange}
-			/>
+			<div className="mb-2 col-xs-12 mb-md-0 col-md-6 col-lg-4">
+				<input
+					className="p-2 col-xs-12 col-md-8"
+					type="text" 
+					placeholder="Search"
+					value={search}
+					onChange={handleChange}
+				/>
+				<button
+					className="btn btn-primary p-2 col-xs-12 col-md-4" 
+					onClick={handleSearch}
+				>
+					Search
+				</button>
+			</div>
       <button 
 				className="btn btn-warning col-xs-12 col-md-3 col-lg-1" 
 				onClick={() => dispatch(logout())}
